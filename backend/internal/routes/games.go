b10db706044436c842abc59d4e9fb21f1d19c9dc
edusantanaw <eduardosantana20122002@gmail.com>
2024-01-07 11:06:11 -0300

    feat: configure jwt authentication

--- backend/internal/routes/games.go
+++ backend/internal/routes/games.go
@@ -1,13 +1,26 @@
 package routes
 
 import (
+	"fmt"
+
 	"github.com/edusantanaw/games_match.git/adapters"
 	"github.com/edusantanaw/games_match.git/internal/controllers"
+	authentication "github.com/edusantanaw/games_match.git/pkg/authentication"
 	structs "github.com/edusantanaw/games_match.git/pkg/utils/structs"
 	"github.com/gin-gonic/gin"
 )
 
 func GameRoutes(route *gin.Engine) {
-	route.POST("/game", adapters.GinAdapter[*controllers.CreateGameData](controllers.RegisterGame))
+	gamesGroup := route.Group("/game")
+	gamesGroup.Use(func(ctx *gin.Context) {
+		userId, err := authentication.ExtractTokenMetadata(ctx)
+		if err != nil {
+			ctx.JSON(401, err.Error())
+			return
+		}
+		fmt.Println(userId)
+		ctx.Next()
+	})
+	gamesGroup.POST("/", adapters.GinAdapter[*controllers.CreateGameData](controllers.RegisterGame))
 	route.GET("/game", adapters.GinAdapter[structs.IPagination](controllers.LoadGames))
 }
---
+++ backend/pkg/authentication/jwt.go
@@ -0,0 +1,73 @@
+package authentication
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var secret = os.Getenv("JWT_SECRET")
+
+func CreateJwtToken(userId string) (string, error) {
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userId
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(secret))
+	return token, err
+}
+
+func ExtractToken(r *gin.Context) (string, error) {
+	bearToken := r.Request.Header.Get("Authorization")
+	splitedToken := strings.Split(bearToken, " ")
+	if splitedToken[0] != "Bearer" {
+		return "", errors.New("Token invalido")
+	}
+	if len(splitedToken) != 2 {
+		return "", errors.New("Token invalido")
+	}
+	return splitedToken[1], nil
+}
+
+func TokenValid(r *gin.Context) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+func ExtractTokenMetadata(r *gin.Context) (string, error) {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return "", err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		userId := fmt.Sprint(claims["user_id"])
+
+		return userId, nil
+	}
+	return "", err
+}
+
+func VerifyToken(r *gin.Context) (*jwt.Token, error) {
+	tokenString, err := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
