5e764b7bdea1b6972f3c7651e9aa9b9ac0b0e951
edusantanaw <eduardosantana20122002@gmail.com>
2024-01-06 21:45:59 -0300

    refactory: move the data validation to entitie core

--- backend/controllers/registerGame.go
+++ backend/controllers/registerGame.go
@@ -1,13 +1,9 @@
 package controllers
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/edusantanaw/games_match.git/db"
 	"github.com/edusantanaw/games_match.git/entities"
 	"github.com/edusantanaw/games_match.git/httpResponse"
-	"github.com/google/uuid"
 )
 
 type CreateGameData struct {
@@ -19,28 +15,10 @@ type CreateGameData struct {
 
 func RegisterGame(data *CreateGameData) httpResponse.HttpResponse {
 	db := db.GetConnection()
-	fmt.Println(data)
-	if data.Name == "" {
-		return httpResponse.BadRequest("O nome é obrigatorio!")
-	}
-	if len(data.Categories) == 0 {
-		return httpResponse.BadRequest("Pelo menos uma categoria deve ser selecionada!")
-	}
-	if data.Description == "" {
-		return httpResponse.BadRequest("A descrição é obrigatoria!")
-	}
-	if len(data.Plataforms) == 0 {
-		return httpResponse.BadRequest("Pelo menos uma plataforma deve ser selecionada!")
-	}
-	parsedCategories, err := json.Marshal(data.Categories)
+	game, err := entities.CreateValidGame(data.Name, data.Categories, data.Description, data.Plataforms)
 	if err != nil {
-		return httpResponse.BadRequest("Erro ao converter array para json")
+		return httpResponse.BadRequest[string](err.Error())
 	}
-	parsedPlataforms, err := json.Marshal(data.Plataforms)
-	if err != nil {
-		return httpResponse.BadRequest("Erro ao converter array para json")
-	}
-	game := &entities.Game{Name: data.Name, Id: uuid.New().String(), Categories: string(parsedCategories), Plataforms: string(parsedPlataforms), Description: data.Description}
-	db.Create(game)
-	return httpResponse.Created[entities.Game](*game)
+	db.Create(&game)
+	return httpResponse.Created[entities.Game](game)
 }
--- backend/entities/game.go
+++ backend/entities/game.go
@@ -1,6 +1,12 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Game struct {
 	gorm.Model
@@ -9,4 +15,34 @@ type Game struct {
 	Categories  string
 	Description string
 	Plataforms  string
+}
+
+func CreateValidGame(name string, categories []string, description string, plataforms []string) (Game, error) {
+	game := Game{}
+	if name == "" {
+		return game, errors.New("O nome é obrigatorio!")
+	}
+	if len(categories) == 0 {
+		return game, errors.New("Pelo menos uma categoria deve ser selecionada!")
+	}
+	if description == "" {
+		return game, errors.New("A descrição é obrigatoria!")
+	}
+	if len(plataforms) == 0 {
+		return game, errors.New("Pelo menos uma plataforma deve ser selecionada!")
+	}
+	parsedCategories, err := json.Marshal(categories)
+	if err != nil {
+		return game, errors.New("Erro ao converter array para json")
+	}
+	parsedPlataforms, err := json.Marshal(plataforms)
+	if err != nil {
+		return game, errors.New("Erro ao converter array para json")
+	}
+	game.Id = uuid.New().String()
+	game.Name = name
+	game.Categories = string(parsedCategories)
+	game.Plataforms = string(parsedPlataforms)
+	game.Description = description
+	return game, nil
 }
